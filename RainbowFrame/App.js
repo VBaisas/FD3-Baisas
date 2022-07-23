9eaaf0e12aceffba0cfc750bed58f8f9d74ff316
VBaisas <baisas.v@gmail.com>
2022-07-22 22:30:34 -0700

    Add RainbowFrame homework

---
+++ RainbowFrame/App.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import RainbowFrame from './components/RainbowFrame';
+
+let colors = ['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
+
+ReactDOM.render(
+  <RainbowFrame colors={colors}>
+    <div className="Content">Hello!</div>
+  </RainbowFrame> 
+  , document.getElementById('container') 
+);
---
+++ RainbowFrame/components/RainbowFrame.js
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+
+import './RainbowFrame.css';
+
+class RainbowFrame extends React.Component {
+
+  render() {
+    let curr = this.props.children;
+    for ( let color of this.props.colors) {
+      curr = <div className='Frame' style={{border:"solid 7px "+color}}>{curr}</div>;
+    };
+
+    return curr
+  }
+
+}
+
+export default RainbowFrame;
