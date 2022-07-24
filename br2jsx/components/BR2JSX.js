789fcf23d862bdb7d990f5b47f9af03f6ac5fa5b
VBaisas <baisas.v@gmail.com>
2022-07-23 21:48:40 -0700

    Add BR2JSX homework

---
+++ br2jsx/components/BR2JSX.js
@@ -0,0 +1,20 @@
+﻿import React from 'react';
+
+import './BR2JSX.css';
+
+class BR2JSX extends React.Component {
+
+  render() {
+    let lines=this.props.text.split(/<br *\/?>/);
+    let lines2=[];
+    for (let i=0; i<lines.length; i++) {
+      if (i)
+        lines2.push(<br/>);
+      lines2.push(lines[i]);
+    }
+    return <div className="Lines">{lines2}</div>;
+    
+  }
+}
+
+export default BR2JSX;
